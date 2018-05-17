28745288d3982e2bff6f5e4e0b306963618d82fa
labcabrera <lab.cabrera@gmail.com>
2018-05-17 12:41:50 +0200

    Readme

--- src/main/java/org/lab/samples/consul/consulsample/rest/ApiConsumerController.java
+++ src/main/java/org/lab/samples/consul/consulsample/rest/ApiConsumerController.java
@@ -2,6 +2,7 @@ package org.lab.samples.consul.consulsample.rest;
 
 import org.lab.samples.consul.consulsample.model.DummyMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -9,12 +10,15 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ApiConsumerController {
 
+	@Value("http://${spring.application.name}/api/v1/dummy")
+	private String endpoint;
+
 	@Autowired
 	private RestTemplate restTemplate;
 
 	@GetMapping("/api/v1/consumer")
 	public DummyMessage consume() {
-		return this.restTemplate.getForObject("http://consul-sample/api/v1/dummy", DummyMessage.class);
+		return restTemplate.getForObject(endpoint, DummyMessage.class);
 	}
 
 }
